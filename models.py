87437131d79cfab9e98ecfd68f9d2a01a56cdead
Derrick Rioba Ongwae <riobad74@gmail.com>
2024-11-21 14:19:25 +0300

    Increased pass length

--- models.py
+++ models.py
@@ -9,7 +9,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(255), unique=True, nullable=False)
     email = db.Column(db.String(255), nullable=False, unique=True)
-    password_hash = db.Column(db.String(255), nullable=False)
+    password_hash = db.Column(db.String(500), nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
     artworks = db.relationship('Artwork', backref='owner', lazy=True)  # Relationship to Artwork
 
