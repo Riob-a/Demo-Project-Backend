073ece8ca84c9fc62c19fece406b3dcb3f14578d
Derrick Rioba Ongwae <riobad74@gmail.com>
2024-11-21 16:29:51 +0300

    lenghen admin hash

--- models.py
+++ models.py
@@ -76,10 +76,10 @@ class Contact(db.Model):
 class Admin(db.Model):
     __tablename__ = 'admins'
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password_hash = db.Column(db.String(128), nullable=False)
-    role = db.Column(db.String(50), nullable=False, default='admin')  # Roles like 'admin', 'superadmin'
+    username = db.Column(db.String(225), unique=True, nullable=False)
+    email = db.Column(db.String(255), unique=True, nullable=False)
+    password_hash = db.Column(db.String(500), nullable=False)
+    role = db.Column(db.String(250), nullable=False, default='admin')  # Roles like 'admin', 'superadmin'
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
 
     def set_password(self, password):
