08eb928468b184a68be519c8965ff420003c9d67
Derrick Rioba Ongwae <riobad74@gmail.com>
2024-11-21 14:11:54 +0300

    changed length

--- models.py
+++ models.py
@@ -7,9 +7,9 @@ db = SQLAlchemy()
 class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    email = db.Column(db.String(120), nullable=False, unique=True)
-    password_hash = db.Column(db.String(128), nullable=False)
+    username = db.Column(db.String(255), unique=True, nullable=False)
+    email = db.Column(db.String(255), nullable=False, unique=True)
+    password_hash = db.Column(db.String(255), nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
     artworks = db.relationship('Artwork', backref='owner', lazy=True)  # Relationship to Artwork
 
