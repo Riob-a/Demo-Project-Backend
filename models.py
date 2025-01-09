ffab714ec8c70b9532dd7326cb091c08a0cdfdb3
Derrick Rioba Ongwae <riobad74@gmail.com>
2025-01-09 22:34:49 +0300

    added artwork  relationship

--- models.py
+++ models.py
@@ -61,15 +61,18 @@ class ArtworkLike(db.Model):
     artwork_id = db.Column(db.Integer, db.ForeignKey('art.id'), nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
 
+    artwork = db.relationship('Artwork', backref=db.backref('likes', lazy=True))
+
     def __repr__(self):
         return f"<ArtworkLike artwork_id={self.artwork_id} user_id={self.user_id}>"
-    
+
     def to_dict(self):
-        return{
-            "id":self.id,
-            "artwork_id":self.artwork_id,
-            "user_id":self.user_id,
+        return {
+            "id": self.id,
+            "artwork_id": self.artwork_id,
+            "user_id": self.user_id,
         }
+
 
 class Contact(db.Model):
     __tablename__='contact'
