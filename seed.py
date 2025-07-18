b9c0859a425363eb6e4944d0eff756fae1e76926
Derrick Rioba Ongwae <riobad74@gmail.com>
2025-07-18 14:16:35 +0300

    edited seed

--- seed.py
+++ seed.py
@@ -8,7 +8,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
 
